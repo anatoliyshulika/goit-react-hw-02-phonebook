200a21b9dc88282b44b72264e6df4eec72241c35
Anatoliy Shulika <shulika.anatoliy2@gmail.com>
2022-12-20 13:59:12 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    {/* <ThemeProvider theme={theme}> */}
+    <App />
+    {/* </ThemeProvider> */}
   </React.StrictMode>
 );
-//qwer
